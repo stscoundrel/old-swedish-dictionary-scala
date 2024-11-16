0c60afc3ae46582d8d049ea89a18ded49def48e6
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-11-16 21:52:43 +0200

    Update dictionary dataset, use gzipped format
    Includes latest fixes from dictionary builder project. Should also take much less space due to gziping.

--- src/main/scala/io/github/stscoundrel/oldswedish/Dictionary.scala
+++ src/main/scala/io/github/stscoundrel/oldswedish/Dictionary.scala
@@ -3,16 +3,18 @@ package io.github.stscoundrel.oldswedish
 import com.google.gson.Gson
 
 import java.io.InputStreamReader
+import java.util.zip.GZIPInputStream
 
 class Dictionary {
-  private final val DICTIONARY_RESOURCE_PATH = "/old-swedish-dictionary.json"
+  private final val DICTIONARY_RESOURCE_PATH = "/old-swedish-dictionary.min.json.gz"
   private lazy val entries: Array[DictionaryEntry] = readDictionary()
 
   def getDictionary(): Array[DictionaryEntry] = entries
 
   private def readDictionary(): Array[DictionaryEntry] = {
     val inputStream = getClass.getResourceAsStream(DICTIONARY_RESOURCE_PATH)
-    val reader = new InputStreamReader(inputStream)
+    val gzipStream = new GZIPInputStream((inputStream))
+    val reader = new InputStreamReader(gzipStream)
 
     val gson = new Gson()
 
