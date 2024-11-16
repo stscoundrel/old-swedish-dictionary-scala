cf00532b95da1dd7a5aec0add000b1db08e2f7d1
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2024-11-16 21:36:41 +0200

    Format code & imports

--- src/main/scala/io/github/stscoundrel/oldswedish/DictionaryEntry.scala
+++ src/main/scala/io/github/stscoundrel/oldswedish/DictionaryEntry.scala
@@ -2,11 +2,13 @@ package io.github.stscoundrel.oldswedish
 
 import com.google.gson.annotations.SerializedName
 
+import scala.annotation.meta.field
+
 case class DictionaryEntry(
-  @(SerializedName @scala.annotation.meta.field)("a") val headword: String,
-  @(SerializedName @scala.annotation.meta.field)("b") val partOfSpeech: Array[String],
-  @(SerializedName @scala.annotation.meta.field)("c") val grammaticalAspect: String,
-  @(SerializedName @scala.annotation.meta.field)("d") val information: String,
-  @(SerializedName @scala.annotation.meta.field)("e") val definitions: Array[String],
-  @(SerializedName @scala.annotation.meta.field)("f") val alternativeForms: Array[String],
+  @(SerializedName@field)("a") val headword: String,
+  @(SerializedName@field)("b") val partOfSpeech: Array[String],
+  @(SerializedName@field)("c") val grammaticalAspect: String,
+  @(SerializedName@field)("d") val information: String,
+  @(SerializedName@field)("e") val definitions: Array[String],
+  @(SerializedName@field)("f") val alternativeForms: Array[String],
 )
